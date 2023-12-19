8e12caa5e661fdab3e2c5d8b7651778d62702f37
Erendiria <151480543+Erendiria@users.noreply.github.com>
2023-12-19 14:36:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from flask_jwt_extended import JWTManager 
 from flask_restful import Api
 from config import Config
-from resources.posting import PostingListResource
+from resources.follow import FollowResource
+from resources.posting import PostingListResource, PostingResource
 from resources.user import UserLoginResource, UserLogoutResource, UserRegisterResource, jwt_blocklist
 
 app = Flask(__name__)
@@ -26,10 +27,11 @@ api.add_resource( UserRegisterResource , '/user/register')
 api.add_resource( UserLoginResource, '/user/login')
 api.add_resource( UserLogoutResource , '/user/logout')
 api.add_resource( PostingListResource , '/posting')
+api.add_resource( FollowResource , '/follow/<int:followee_id>')
+api.add_resource( PostingResource , '/posting/<int:posting_id>')
 
 if __name__ == '__main__':
     app.run()
-
 
 
 
